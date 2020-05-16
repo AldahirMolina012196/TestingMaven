41e44e71b6c618451e80fcf6c5004f6275317b0b
Aldahir M <aldahir_molina82161@elpoli.edu.co>
2020-05-16 12:26:54 -0500

    Testing maven

---
+++ src/main/java/com/co/politecnicojic/pruebasmaven/demo/PruebasmavenApplication.java
@@ -0,0 +1,13 @@
+package com.co.politecnicojic.pruebasmaven.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebasmavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebasmavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/co/politecnicojic/pruebasmaven/demo/PruebasmavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.politecnicojic.pruebasmaven.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebasmavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
